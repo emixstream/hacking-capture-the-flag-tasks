10626770d1538372cb75e7c8a2b2ee92dccd6415
Emanuele Marnati <72609077+emixstream@users.noreply.github.com>
2021-12-10 12:13:14 +0100

    Add files via upload

---
+++ rabbit_hole/get_something.js
@@ -0,0 +1,12 @@
+
+
+const base65536 = require('base65536')
+const fs = require('fs')
+
+
+data = fs.readFileSync('rabbithole_characters.txt')
+//.toString('utf-8')
+//decoded = new Uint8Array.decode(base65536.decode(data))
+
+// console.log(decoded)
+//fs.writeFileSync('new_data', decoded)
